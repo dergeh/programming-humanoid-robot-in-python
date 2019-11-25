8e7a2946aeb35b3d38303dd80613daa8673fff9a
durgen <richard.girke@campus.tu-berlin.de>
2019-11-25 17:13:55 +0100

    interpolation added

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -21,7 +21,7 @@
 
 
 from pid import PIDAgent
-from keyframes import hello
+from keyframes import hello, leftBellyToStand, wipe_forehead, leftBackToStand
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -32,6 +32,7 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.t0=0 # starting time of the agent gotten from simspark
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -40,11 +41,63 @@ class AngleInterpolationAgent(PIDAgent):
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
-        # YOUR CODE HERE
+        names, times, keys = keyframes
+        
+        # iterates over the joint names to interpolate the value for the given joint
+        for i in range(0,len(names)):
+                time_index=0
+                #time since the agent started
+                time_rel=self.perception.time-self.t0
 
+                #bool for termination if time limit is reached
+                end=False
+
+                # iterates over all times to determine timeframe we are in
+                for it in range (0, len(times[i])):
+                    
+                    if times[i][it] > time_rel:
+                        time_index=it
+                        break
+                    if it==len(times[i])-1:
+                        target_joints[names[i]]=keys[i][it][0]
+                        end=True
+                # if end of times array is reached robot halts at last given position
+                if end:
+                    continue
+                # if index 0 start with 0,0 as position to avoid index faults
+                if time_index==0:
+                    p0=(0,0)
+                # else get pervious position
+                else:
+                    p0=(0,keys[i][time_index-1][0])
+               
+                # set point for bezier interpolation
+                p1=(keys[i][time_index][1][1],keys[i][0][1][2]) # control point 1
+                p2=(keys[i][time_index][2][1],keys[i][0][2][2]) # control point 2
+                p3=(1,keys[i][time_index][0]) # destination
+
+                # normalize time so t gets between 0 and 1
+                lower=0
+                if time_index!=0:
+                    lower=times[i][time_index-1]
+                upper=times[i][time_index]
+                t=(time_rel-lower)/(upper-lower)
+
+                # cubic bezier
+                n_p0=tuple(x*((1-t)**3) for x in p0)
+                n_p1=tuple(x*3*t*((1-t)**2) for x in p1 )
+                n_p2=tuple(x*3*(1-t)*(t**2) for x in p2) 
+                n_p3=tuple(x*(t**3) for x in p3 )
+                b=(n_p0[0]+n_p1[0]+n_p2[0]+n_p3[0], n_p0[1]+n_p1[1]+n_p2[1]+n_p3[1])  
+
+                #add interpolated value to target_joint dicct
+                target_joints[names[i]]=b[1]
+                
+       
         return target_joints
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBellyToStand() # CHANGE DIFFERENT KEYFRAMES
+    agent.t0=agent.perception.time # set starting time to get relative time in interpolation
     agent.run()
